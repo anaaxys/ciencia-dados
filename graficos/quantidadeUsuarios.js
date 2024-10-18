37dfc955996d82a08b4a6279fe6197c4c629e306
Ana Alura <105944221+anaaxys@users.noreply.github.com>
2024-10-18 11:09:17 -0300

    Add files via upload

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,3 +1,4 @@
+import { getCSS } from "./common.js"
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch (url)
@@ -10,8 +11,15 @@ async function quantidadeUsuarios() {
             x: nomeDasRedes,
             y: quantidadeUsuarios,
             type: 'bar'
+            marker: {
+                color: getCSS('--primary-color')
+            }
         }
     ]
+
+    const layout ={
+        plot_bgcolor
+    }
         const grafico = document.createElement ('div')
         grafico.className = 'grafico'
         document.getElementById ('graficos-container').appendChild(grafico)
