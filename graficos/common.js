85d0d0ceb4750f11d9769379301908ab6d67b1a4
Ana Alura <105944221+anaaxys@users.noreply.github.com>
2024-11-22 11:08:04 -0300

    Add files via upload

--- graficos/common.js
+++ graficos/common.js
@@ -7,4 +7,12 @@ const tickConfig = {
     size: 16,
     family: getCSS('--font')
 }
+
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+
 export {getCSS, tickConfig}
