0715c5dd50c53cca0c5bc4fef83e620253afaaec
Ana Alura <105944221+anaaxys@users.noreply.github.com>
2024-11-28 18:42:31 -0300

    Add files via upload

--- graficos/redesFavoritasMundo.js
+++ graficos/redesFavoritasMundo.js
@@ -1,7 +1,7 @@
-import { getCSS } from "./common.js";
+import { getCSS, criarGrafico, incluirTexto } from "./common.js";
 
 async function redesFavoritasMundo () {
-    const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json";
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json';
     const res = await fetch(url);
     const dados = await res.json();
     const redes = Object.keys(dados);
@@ -11,10 +11,10 @@ async function redesFavoritasMundo () {
         {
             values: valores,
             labels: redes,
-            type: 'pie'
-            textinfo: "label+percent",
-        },
-    ];
+            type: 'pie',
+            textinfo: 'label+percent'
+        }
+    ]
 
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
@@ -27,17 +27,22 @@ async function redesFavoritasMundo () {
                 color: getCSS('--primary-color'),
                 family: getCSS('--font'),
                 size: 30
-            },
+            }
         },
        legend: {
             font: {
                 color: getCSS (--primary-color),
                 size: 16
-            },
-       },
-    };
+            }
+       }
+    }
 
    criarGrafico (data, layout)
+   
+   incluirTexto(`Embora o <span>Instagram</span> ocupe a quarta posição em termos de número total de usuários 
+    entre as redes sociais, destaca-se como a <span>preferida pelos usuários</ span>. Supera até 
+    mesmo o <span>Facebook</span>, a plataforma com mais usuários, sendo a terceira opção mais apreciada pelos usuários. <br>Essa 
+    preferência evidencia a forte conexão e apreço que as pessoas têm pelo Instagram em comparação com outras redes sociais.`)
 }
 
-redesFavoritasMundo ();
+redesFavoritasMundo ()
